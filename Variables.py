669f91d77fa89660cb7b686fb08c522dc40073c6
Kev <105522526+KevoxChrist@users.noreply.github.com>
2024-12-19 11:26:42 -0500

    Create Variables.py
    Variables such as Strings, Integers, Floats, & Booleans

---
+++ Variables.py
@@ -0,0 +1,12 @@
+#Variables (String, Integer, Float, & Boolean)
+first_name = "Pierce"
+birth_year = 2003
+weight = 189.6
+gen_z = True
+christian_status = True
+
+print(f"Hello {first_name}!")
+print(f"You were born in {birth_year}.")
+print(f"Your weight is {weight}!")
+print(f"You are a part of Gen Z: {gen_z}!")
+print(f"You are a Christian: {christian_status}!")

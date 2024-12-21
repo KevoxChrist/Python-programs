c99fe6be820897e5d2d9d8ef6a743be22254a9c6
Kev <105522526+KevoxChrist@users.noreply.github.com>
2024-12-20 19:11:27 -0500

    Create Madlibs.py
    A funny madlibs game!

---
+++ Madlibs.py
@@ -0,0 +1,27 @@
+#Madlibs Game :)
+
+adjective1 = input("Enter an adjective (a word that describes something):")
+villain = input("Enter the name of a villain:")
+adjective2 = input("Enter an adjective (a word that describes something):")
+object1 = input("Enter the name of a object:")
+verb1 = input("Enter the name of a verb (a action word) ending in 'ing':")
+noun1 = input("Enter a noun (people, place, or thing) that's plural:")
+object2 = input("Enter the name of an object:")
+adjective3 = input("Enter an adjective (a word that describes something):")
+food = input("Enter a food item:")
+verb2 = input("Enter a verb (a action word) that's singular:")
+adjective4 = input("Enter an adjective (a word that describes something):")
+device_machine = input("Enter any type of device or machine:")
+noun2 = input("Enter a noun (people, place, or thing) that's plural:")
+
+print("")
+print(f"During a {adjective1} battle with {villain}, Megaman accidentally equipped"
+      f" his {adjective2} {object1} instead of his buster cannon.")
+print(f"“Oh no!” he shouted, as the weapon started {verb1} and"
+      f" launched {noun1} everywhere.")
+print(f"The villain laughed so hard they dropped their {object2} and slipped on a"
+      f" {adjective3} {food}.")
+print(f"Megaman used the chance to {verb2} away and found Dr. Light tinkering with"
+      f" a {adjective4} {device_machine}.")
+print(f"“Megaman,” Dr. Light said, “we need to stop fighting robots and start"
+      f" fighting {noun2}!”")
